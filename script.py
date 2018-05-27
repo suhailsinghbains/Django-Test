26b0afa164308c6c7cfa7d7a141c9fd12a883122
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-27 22:04:53 +0530

    Building with Django

--- script.py
+++ script.py
@@ -4,4 +4,5 @@ from mysite.views import hello, current_datetime
 urlpatterns = patterns('',
     ('^hello/$', hello),
     ('^time/$', current_datetime),
+    ('^another-time-page/$', current_datetime),
 )
