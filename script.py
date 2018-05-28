6964ebfb747e75174ee9c879693ad5852bde4a4c
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-28 21:58:59 +0530

    Building with Django

--- script.py
+++ script.py
@@ -2,7 +2,9 @@ from django.conf.urls.defaults import *
 from mysite.views import hello, current_datetime
 
 urlpatterns = patterns('',
-    ('^hello/$', hello),
     ('^time/$', current_datetime),
-    ('^another-time-page/$', current_datetime),
+    ('^time/plus/1/$', one_hour_ahead),
+    ('^time/plus/2/$', two_hours_ahead),
+    ('^time/plus/3/$', three_hours_ahead),
+    ('^time/plus/4/$', four_hours_ahead),
 )
