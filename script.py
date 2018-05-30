ab84591a37d910ceb08489f53c8d75c9a5592d5a
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-05-30 19:59:33 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
-from django.conf.urls.defaults import *
-from mysite.views import hello, current_datetime
+from django.http import Http404, HttpResponse
+import datetime
 
-urlpatterns = patterns('',
-    ('^time/$', current_datetime),
-    ('^time/plus/1/$', one_hour_ahead),
-    ('^time/plus/2/$', two_hours_ahead),
-    ('^time/plus/3/$', three_hours_ahead),
-    ('^time/plus/4/$', four_hours_ahead),
-)
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
