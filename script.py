e029e35fb30f96a53fb2b3fe01e3ab835a0c677c
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-06-06 22:34:44 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,13 +1,10 @@
-from django.template import Template, Context
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 import datetime
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    # Simple way of using templates from the filesystem.
-    # This is BAD because it doesn't account for missing files!
-    fp = open('/home/djangouser/templates/mytemplate.html')
-    t = Template(fp.read())
-    fp.close()
+    t = get_template('current_datetime.html')
     html = t.render(Context({'current_date': now}))
     return HttpResponse(html)
