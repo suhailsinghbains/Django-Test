f7f2228dcde55b35b983363b52f1bc6b61d71900
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-06-07 22:55:08 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,10 +1,6 @@
-from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 import datetime
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
-    return HttpResponse(html)
+    return render_to_response('current_datetime.html', {'current_date': now})
