d6c8d6844e1f7751d1ddaf19be3b72a17e4cea37
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-06-13 12:52:23 -0700

    Building with Django

--- script.py
+++ script.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
-
-def search_form(request):
-    return render(request, 'search_form.html')
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
