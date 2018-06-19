debd648bc19a0c8c9dffc454d3f311eae3ad9ead
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-06-19 23:40:13 +0430

    Building with Django

--- script.py
+++ script.py
@@ -1,11 +1,7 @@
 from django.conf.urls.defaults import *
+from mysite import views
 
-urlpatterns = patterns('mysite.views',
-    (r'^hello/$', 'hello'),
-    (r'^time/$', 'current_datetime'),
-    (r'^time/plus/(\d{1,2})/$', 'hours_ahead'),
-)
-
-urlpatterns += patterns('weblog.views',
-    (r'^tag/(\w+)/$', 'tag'),
+urlpatterns = patterns('',
+    (r'^articles/(?P<year>\d{4})/$', views.year_archive),
+    (r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/$', views.month_archive),
 )
