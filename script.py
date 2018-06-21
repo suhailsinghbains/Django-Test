4cb86cce79362aede6db796e7bdf2cfc90b65eb5
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-06-21 23:30:19 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,22 +1,8 @@
-# urls.py
-
 from django.conf.urls.defaults import *
-from mysite import views
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    (r'^foo/$', views.foo_view),
-    (r'^bar/$', views.bar_view),
+    (r'^about/$', direct_to_template, {
+        'template': 'about.html'
+    })
 )
-
-# views.py
-
-from django.shortcuts import render
-from mysite.models import MyModel
-
-def foo_view(request):
-    m_list = MyModel.objects.filter(is_new=True)
-    return render(request, 'template1.html', {'m_list': m_list})
-
-def bar_view(request):
-    m_list = MyModel.objects.filter(is_new=True)
-    return render(request, 'template2.html', {'m_list': m_list})
