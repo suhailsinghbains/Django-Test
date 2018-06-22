6466a3f32de7fd4053e609479cdad4856511f3b6
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-06-22 19:21:27 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,8 +1,14 @@
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    (r'^about/$', direct_to_template, {
-        'template': 'about.html'
-    })
+    (r'^(?P<username>\w+)/blog/', include('foo.urls.blog')),
+)
+
+# foo/urls/blog.py
+
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    (r'^$', 'foo.views.blog_index'),
+    (r'^archive/$', 'foo.views.blog_archive'),
 )
