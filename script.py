89a8764b8ec5c7ab23f9db28170ef0f28f09fcb6
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-06-27 21:02:41 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,15 +1,5 @@
-from django.db import connection, models
+from django.http import HttpResponse
 
-class PersonManager(models.Manager):
-    def first_names(self, last_name):
-        cursor = connection.cursor()
-        cursor.execute("""
-            SELECT DISTINCT first_name
-            FROM people_person
-            WHERE last_name = %s""", [last_name])
-        return [row[0] for row in cursor.fetchone()]
-
-class Person(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    objects = PersonManager()
+def my_image(request):
+    image_data = open("/path/to/my/image.png", "rb").read()
+    return HttpResponse(image_data, mimetype="image/png")
