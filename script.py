93ba74b60a7937ac85e23a47bc9bc6c15210c2b5
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-06-27 21:01:56 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,28 +1,15 @@
-from django.contrib.localflavor.us.models import USStateField
-from django.db import models
+from django.db import connection, models
+
+class PersonManager(models.Manager):
+    def first_names(self, last_name):
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT DISTINCT first_name
+            FROM people_person
+            WHERE last_name = %s""", [last_name])
+        return [row[0] for row in cursor.fetchone()]
 
 class Person(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    birth_date = models.DateField()
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=50)
-    state = USStateField() # Yes, this is U.S.-centric...
-
-    def baby_boomer_status(self):
-        "Returns the person's baby-boomer status."
-        import datetime
-        if datetime.date(1945, 8, 1) <= self.birth_date <= datetime.date(1964, 12, 31):
-            return "Baby boomer"
-        if self.birth_date < datetime.date(1945, 8, 1):
-            return "Pre-boomer"
-        return "Post-boomer"
-
-    def is_midwestern(self):
-        "Returns True if this person is from the Midwest."
-        return self.state in ('IL', 'WI', 'MI', 'IN', 'OH', 'IA', 'MO')
-
-    def _get_full_name(self):
-        "Returns the person's full name."
-        return u'%s %s' % (self.first_name, self.last_name)
-    full_name = property(_get_full_name)
+    objects = PersonManager()
