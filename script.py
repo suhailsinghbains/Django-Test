da2fa72e05e76c137dcd835f1de3d73dfe9c7b72
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-07-01 23:35:46 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,25 +1,10 @@
-from cStringIO import StringIO
-from reportlab.pdfgen import canvas
-from django.http import HttpResponse
+from django.contrib.syndication.feeds import Feed
+from mysite.blog.models import Entry
 
-def hello_pdf(request):
-    # Create the HttpResponse object with the appropriate PDF headers.
-    response = HttpResponse(mimetype='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename=hello.pdf'
+class LatestEntries(Feed):
+    title = "My Blog"
+    link = "/archive/"
+    description = "The latest news about stuff."
 
-    temp = StringIO()
-
-    # Create the PDF object, using the StringIO object as its "file."
-    p = canvas.Canvas(temp)
-
-    # Draw things on the PDF. Here's where the PDF generation happens.
-    # See the ReportLab documentation for the full list of functionality.
-    p.drawString(100, 100, "Hello world.")
-
-    # Close the PDF object cleanly.
-    p.showPage()
-    p.save()
-
-    # Get the value of the StringIO buffer and write it to the response.
-    response.write(temp.getvalue())
-    return response
+    def items(self):
+        return Entry.objects.order_by('-pub_date')[:5]
