a0216ef5d7037a9ac7cc4379075ad38dc60aae31
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-07-03 23:36:44 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,23 +1,14 @@
-from django.core.exceptions import ObjectDoesNotExist
-from mysite.blog.models import Entry, Tag
+from django.contrib.syndication.feeds import Feed
+from django.utils.feedgenerator import Atom1Feed
+from mysite.blog.models import Entry
 
-class TagFeed(Feed):
-    def get_object(self, bits):
-        # In case of "/feeds/tags/cats/dogs/mice/", or other such
-        # clutter, check that bits has only one member.
-        if len(bits) != 1:
-            raise ObjectDoesNotExist
-        return Tag.objects.get(tag=bits[0])
+class RssLatestEntries(Feed):
+    title = "My Blog"
+    link = "/archive/"
+    description = "The latest news about stuff."
 
-    def title(self, obj):
-        return "My Blog: Entries tagged with %s" % obj.tag
+    def items(self):
+        return Entry.objects.order_by('-pub_date')[:5]
 
-    def link(self, obj):
-        return obj.get_absolute_url()
-
-    def description(self, obj):
-        return "Entries tagged with %s" % obj.tag
-
-    def items(self, obj):
-        entries = Entry.objects.filter(tags__id__exact=obj.id)
-        return entries.order_by('-pub_date')[:30]
+class AtomLatestEntries(RssLatestEntries):
+    feed_type = Atom1Feed
