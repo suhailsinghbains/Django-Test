e8ea405a45d9fd0e100fb2970eba1fb8a89a38ba
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-07-09 21:01:40 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,6 +1,15 @@
-def show_color(request):
-    if "favorite_color" in request.COOKIES:
-        return HttpResponse("Your favorite color is %s" % \
-            request.COOKIES["favorite_color"])
+def set_color(request):
+    if "favorite_color" in request.GET:
+
+        # Create an HttpResponse object...
+        response = HttpResponse("Your favorite color is now %s" % \
+            request.GET["favorite_color"])
+
+        # ... and set a cookie on the response
+        response.set_cookie("favorite_color",
+                            request.GET["favorite_color"])
+
+        return response
+
     else:
-        return HttpResponse("You don't have a favorite color.")
+        return HttpResponse("You didn't give a favorite color.")
