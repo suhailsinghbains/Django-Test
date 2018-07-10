2194bcc8a44df633faf4ee229a2f446645f6a9e3
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-07-10 21:01:59 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,15 +1,13 @@
-def set_color(request):
-    if "favorite_color" in request.GET:
+# Set a session value:
+request.session["fav_color"] = "blue"
 
-        # Create an HttpResponse object...
-        response = HttpResponse("Your favorite color is now %s" % \
-            request.GET["favorite_color"])
+# Get a session value -- this could be called in a different view,
+# or many requests later (or both):
+fav_color = request.session["fav_color"]
 
-        # ... and set a cookie on the response
-        response.set_cookie("favorite_color",
-                            request.GET["favorite_color"])
+# Clear an item from the session:
+del request.session["fav_color"]
 
-        return response
-
-    else:
-        return HttpResponse("You didn't give a favorite color.")
+# Check if the session has a given key:
+if "fav_color" in request.session:
+    ...
