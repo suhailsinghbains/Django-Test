dae69076d63d82ec1a7976426f3f50da0b9411ad
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-07-12 21:02:49 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,14 +1,10 @@
-def post_comment(request):
+def login(request):
     if request.method != 'POST':
         raise Http404('Only POSTs are allowed')
-
-    if 'comment' not in request.POST:
-        raise Http404('Comment not submitted')
-
-    if request.session.get('has_commented', False):
-        return HttpResponse("You've already commented.")
-
-    c = comments.Comment(comment=request.POST['comment'])
-    c.save()
-    request.session['has_commented'] = True
-    return HttpResponse('Thanks for your comment!')
+    try:
+        m = Member.objects.get(username=request.POST['username'])
+        if m.password == request.POST['password']:
+            request.session['member_id'] = m.id
+            return HttpResponseRedirect('/you-are-logged-in/')
+    except Member.DoesNotExist:
+        return HttpResponse("Your username and password didn't match.")
