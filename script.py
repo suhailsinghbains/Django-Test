51838150311c747d4b0a922baae71bb942157459
suhailsinghbains <suhail.singh.bains@hotmail.com>
2018-07-13 00:13:38 +0530

    Building with Django

--- script.py
+++ script.py
@@ -1,10 +1,23 @@
 def login(request):
-    if request.method != 'POST':
-        raise Http404('Only POSTs are allowed')
-    try:
-        m = Member.objects.get(username=request.POST['username'])
-        if m.password == request.POST['password']:
-            request.session['member_id'] = m.id
-            return HttpResponseRedirect('/you-are-logged-in/')
-    except Member.DoesNotExist:
-        return HttpResponse("Your username and password didn't match.")
+
+    # If we submitted the form...
+    if request.method == 'POST':
+
+        # Check that the test cookie worked (we set it below):
+        if request.session.test_cookie_worked():
+
+            # The test cookie worked, so delete it.
+            request.session.delete_test_cookie()
+
+            # In practice, we'd need some logic to check username/password
+            # here, but since this is an example...
+            return HttpResponse("You're logged in.")
+
+        # The test cookie failed, so display an error message. If this
+        # were a real site, we'd want to display a friendlier message.
+        else:
+            return HttpResponse("Please enable cookies and try again.")
+
+    # If we didn't post, send the test cookie along with the login form.
+    request.session.set_test_cookie()
+    return render_to_response('foo/login_form.html')
